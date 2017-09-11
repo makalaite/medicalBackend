1820972c2fe830a5b61e91893aac964b21b189f0
Agne <agnepetrylaite@gmail.com>
2017-09-11 14:42:57 +0300

    form with auth and middleware

---
+++ app/Http/Controllers/MAUsersController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MAUsers;
+use Illuminate\Http\Request;
+use Namshi\JOSE\JWS;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
+
+class MAUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $user = JWTAuth::parseToken()->toUser();
+        $users = MAUsers::all();
+        $response = [
+            'users' => $users,
+            'user' => $user
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //frontend only
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //frontend only
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function signin(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials!'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+        return response()->json(['token' => $token], 200);
+
+    }
+}
---
+++ app/Models/MAUsers.php
@@ -0,0 +1,23 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class MAUsers extends Authenticatable
+{
+    use Notifiable;
+    use SoftDeletes;
+    public $incrementing = false;
+
+
+    protected $table = 'ma_users';
+    use Notifiable;
+    protected $fillable = ['id', 'role_id', 'avatar_id', 'first_name', 'last_name', 'position', 'email', 'show_in_front'];
+
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
+}
